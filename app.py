5908461b90fa5336e5bfa7273ac82c87acda3806
rohithsai22 <c.h.rihith@gmail.com>
2026-02-11 17:54:06 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template,
+ request, redirect, session, url_for
+ from werkzeug.security import
+ generate_password_hash,check_password_hash
 import sqlite3
 import smtplib
 from email.message import EmailMessage
 
 app = Flask(__name__)
+app.secret_key = "bhavya_super_securet_key"
 
 
 # ---------- DATABASE ----------
@@ -68,6 +72,23 @@ def send_email(name, email, mobile, message):
 @app.route("/")
 def home():
     return render_template("index.html")
+     @app.route("/services")
+def services():
+    return render_template("services.html")
+
+
+@app.route("/web-development")
+def web_development():
+    return render_template("web.html")
+
+
+@app.route("/app-development")
+def app_development():
+    return render_template("app.html")
+    @app.route("/deployment")
+def deployment():
+    return render_template("deployment.html")
+
 
 
 # ---------- REQUEST ----------
@@ -90,15 +111,38 @@ def request_page():
 # ---------- ADMIN ----------
 @app.route("/admin")
 def admin():
+    if not session.get("admin"):
+        return redirect("/admin-login")
+    return render_template("admin.html")
     conn = sqlite3.connect("database.db")
     cur = conn.cursor()
     cur.execute("SELECT * FROM requests")
     data = cur.fetchall()
     conn.close()
     return render_template("admin.html", data=data)
+    @app.route("/admin-login", methods=["GET", "POST"])
+def admin_login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if username == admin_username and
+         check_password_hash(admin_password_hash, password):
+            session["admin"] = True
+            return redirect("/admin")
+        else:
+            return "Invalid Username or Password"
+
+    return render_template("admin_login.html")
+     # Create admin credentials
+admin_username = "admin"
+admin_password_hash = generate_password_hash("88851")  
+# change password
+@app.route("/logout")
+def logout():
+    session.pop("admin", None)
+    return redirect("/")
 
 
 # ---------- RUN ----------
 if __name__== "__main__":
-     app.run()
-            
