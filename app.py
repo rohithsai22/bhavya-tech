9c7ca51689666454eb19a659a05be2b90077e671
rohithsai22 <c.h.rihith@gmail.com>
2026-02-13 21:38:20 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect, session, url_for
- from werkzeug.security import
+from flask import Flask
+from werkzeug.security import generate_password_hash, check_password_has
  generate_password_hash,check_password_hash
 import sqlite3
 import smtplib
